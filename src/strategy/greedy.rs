bc4a1483e9f6ca015b9497d1e1dddd851fcb6bcd
Antonin KLOPP-TOSSER <antonin.klopp-tosser@grenoble-inp.org>
2018-03-15 20:21:05 +0100

    Implementation de greedy

--- src/strategy/greedy.rs
+++ src/strategy/greedy.rs
@@ -16,6 +16,17 @@ impl fmt::Display for Greedy {
 
 impl Strategy for Greedy {
     fn compute_next_move(&mut self, state: &Configuration) -> Option<Movement> {
-        unimplemented!("TODO: algo glouton")
+        // Iterer sur tous les pions et sur toutes les positions possibles
+        // Regarder le max de pions récupérés
+
+        // Variable du mouvement du joueur
+        let (config, _) = state
+            .movements()
+            .map(|m| (m, state.play(&m).value()))
+            .max_by_key(|&(_, val)| val)
+            .unwrap();
+
+        // Valeur du mouvement de retour.
+        Some(config)
     }
 }
